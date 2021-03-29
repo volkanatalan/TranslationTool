85cee04a350b96ed26cb378c143350c0cb3f65ab
Volkan Atalan <volkanatalan35@gmail.com>
2021-03-29 15:47:15 +0300

    butonlar eklendi

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<div class="d-flex p-2 flex-row-reverse">
+  <button class="btn btn-success ">
+    Kategori Ekle
+  </button>
+</div>
+
 <?php
-
-echo "
-<meta charset=\"utf-8\">
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
-";
-
 $langsPath = './langs/';
 $langFiles = array_slice(scandir($langsPath), 2);
 // echo json_encode($langFiles);
@@ -18,7 +23,16 @@ if (count($langFiles) > 0) {
   // var_dump($firstFileContent['shortMonths']);
 
   foreach ($firstFileContent as $categoryKey => $category) {
-    echo "<h3 class='categoryTitle'>$categoryKey</h3>";
+    echo "
+      <div class='d-flex p-2 flex-row'>
+        <div class='d-flex' style='flex: 1'>
+          <h3 class='categoryTitle'>$categoryKey</h3>
+        </div>
+        <button class='btn btn-success'>
+        +
+        </button>
+      </div>
+    ";
 
     $categoryTranslations = $firstFileContent[$categoryKey];
     // $categoryTranslationsKeys = array_keys($categoryTranslations);
@@ -42,75 +56,118 @@ if (count($langFiles) > 0) {
   }
 }
 
-echo "
+?>
 
-  <!-- Modal -->
-  <div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
-  <div class=\"modal-dialog\" role=\"document\">
-    <div class=\"modal-content\">
-      <div class=\"modal-header\">
-        <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>
-        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">
-          <span aria-hidden=\"true\">&times;</span>
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editModalLabel">Çevirileri Düzenle</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class=\"modal-body\">
+      <div class="modal-body">
       </div>
-      <div class=\"modal-footer\">
-        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>
-        <button type=\"button\" class=\"btn btn-primary\">Save changes</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+        <button type="button" class="btn btn-primary" id="saveTranslationChangesButton">Kaydet</button>
       </div>
     </div>
   </div>
 </div>
+<!-- /Modal -->
 
-  
-  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
-  <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>
-  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>
 
-  <script>
-    function onPressTranslation(key){
-      console.log(key)
+<!-- Modal -->
+<div class="modal fade" id="addCategoryModal" tabindex="-1" role="dialog" aria-labelledby="addCategoryModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addCategoryModalLabel">Kategori Ekle</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+        <button type="button" class="btn btn-primary" id="saveTranslationChangesButton">Kaydet</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /Modal -->
 
-      $.post(\"./service.php\", {q: 'getAllTranslationsByKey', key}, function(data){
-        data = JSON.parse(data)
-        console.log(data)
-        $(\".modal-body\").html(generateModalHtml(data))
-        $(\"#exampleModal\").modal()
-      })
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script>
+  let lastChosenTranslationKey = null
+
+  var saveTranslationChangesButton = document.getElementById("saveTranslationChangesButton");
+  saveTranslationChangesButton.addEventListener("click", function(event) {
+    $("#editModal").modal('hide')
+    var inputLength = $('.translationInput').length
+    var translations = []
+    for(var i = 0; i < inputLength; i++){
+      var value = $('.translationInput').eq(i).val()
+      var file = $('.translationInput').eq(i).attr('file') + '.json'
+      translations.push({file, value})
+      console.log('value:', value)
     }
 
-    function generateModalHtml(data){
-      let html = ''
-      
-      data.forEach(function(language, index){
-        html += `
-        <div class=\"input-group mb-3\">
-          <div class=\"input-group-prepend\">
-            <span class=\"input-group-text\" id=\"basic-addon1\">` + language.lang + `</span>
-          </div>
-          <input type=\"text\" class=\"form-control\" aria-describedby=\"basic-addon1\" value=\"` + language.translation + `\">
-        </div>
-        `
-      })
+    $.post("./service.php", { q: 'updateTranslations', key: lastChosenTranslationKey, translations }, function(data){
+      console.log(data)
+    })
+  })
 
-      return html
-    }
-  </script>
+  function onPressTranslation(key){
+    console.log(key)
 
-  <style>
-  html{
+    lastChosenTranslationKey = key
+
+    $.post("./service.php", {q: 'getAllTranslationsByKey', key}, function(data){
+      console.log(data)
+      data = JSON.parse(data)
+      $("#exampleModal, .modal-body").html(generateModalHtml(data))
+      $("#editModal").modal()
+    })
+  }
+
+  function generateModalHtml(data){
+    let html = ''
     
-  }
+    data.forEach(function(language, index){
+      html += `
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">` + language.lang + `</span>
+        </div>
+        <input type="text" class="form-control translationInput" aria-describedby="basic-addon1" file=` + language.lang + ` value="` + language.translation + `">
+      </div>
+      `
+    })
 
-  .categoryTitle{
-    padding: 0px 20px 0px 20px;
+    return html
   }
+</script>
 
-  .translationRow{
-    padding: 5px 20px 5px 20px;
-    cursor: pointer;
-  }
-  </style>
-";
+<style>
+html{
+  
+}
+
+.categoryTitle{
+  padding: 0px 10px 0px 10px;
+}
+
+.translationRow{
+  padding: 5px 20px 5px 20px;
+  cursor: pointer;
+}
+</style>
--- service.php
+++ service.php
@@ -1,16 +1,52 @@
 <?php
 
+$langsPath = './langs/';
+
 if ($_POST['q']) {
   $q = $_POST['q'];
 
   if ($q == 'getAllTranslationsByKey') {
     echo json_encode(getAllTranslationsByKey($_POST['key']));
   }
+  else if ($q == 'updateTranslations') {
+    updateTranslations();
+  }
+}
+
+
+
+function updateTranslations(){
+  global $langsPath;
+
+  $key = $_POST['key'];
+  $translations = $_POST['translations'];
+  
+  foreach ($translations as $translation) {
+    $filePath = $langsPath . $translation['file'];
+    $fileContent = json_decode(file_get_contents($filePath), true);
+    $keyParts = explode('.', $key);
+    $keyPartsLength = count($keyParts);
+
+    if($keyPartsLength === 1){
+      $fileContent[$keyParts[0]] = $translation['value'];
+    }
+    else if($keyPartsLength === 2){
+      $fileContent[$keyParts[0]][$keyParts[1]] = $translation['value'];
+    }
+
+    echo '$keyParts:' . json_encode($fileContent[$keyParts[0]][$keyParts[1]]) . '----';
+    
+    file_put_contents($filePath, json_encode($fileContent));
+  }
+
+  echo json_encode([
+    "code" => 200
+  ]);
 }
 
 
 function getAllTranslationsByKey($key) {
-  $langsPath = './langs/';
+  global $langsPath;
   $langFiles = array_slice(scandir($langsPath), 2);
   $result = [];
 
